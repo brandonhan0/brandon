fecf0d8ca026dd524aec2ab5592a90521a5bed42
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-21 14:31:22 -0800

    Change logo image in LoginScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ export default function App() {
       <LoginScreen
         onEnter={() => setEntered(true)}
         backgroundImage={loginBg}
-        logoImage={logo}
+        logoImage={skull}
         showScanlines={false}
       />
     );
