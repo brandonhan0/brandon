18d3e9959a051faa6e42317746af41081cbf4912
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-21 13:27:18 -0800

    Remove button options from WorkContent component
    Removed the button options for Save, Print, and Details.

--- src/components/content/WorkContent.tsx
+++ src/components/content/WorkContent.tsx
@@ -219,12 +219,6 @@ function WorkContent() {
                       ))}
                     </ul>
                   </div>
-
-                  <div className="pt-2 flex flex-wrap gap-2">
-                    <span className={retroBtn}>Save</span>
-                    <span className={retroBtn}>Print</span>
-                    <span className={retroBtn}>Details</span>
-                  </div>
                 </div>
               </div>
             </div>
