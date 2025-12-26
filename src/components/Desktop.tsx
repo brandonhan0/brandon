3b2a4d6b250cfb86667f7c1045b5c1db614471ca
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-25 17:12:38 -0800

    Update Desktop.tsx

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { AppType } from "../App";
-import skull from "../assets/icons/skull.gif";
+import skull from "../assets/icons/toothless.gif";
 import kiki from "../assets/icons/kiki.gif";
 import car from "../assets/icons/car.gif";
 import spongebob from "../assets/icons/spongebob.gif";
