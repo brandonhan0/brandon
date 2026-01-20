71958106711bbbbddc0ef24fc9668afb08512b44
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-01-20 13:34:52 -0800

    Refine project description and achievements in WorkContent

--- src/components/content/WorkContent.tsx
+++ src/components/content/WorkContent.tsx
@@ -36,11 +36,11 @@ function WorkContent() {
         company: "calpoly racing",
         period: "2025 - now",
         description:
-          "leading subteam of firmware engineers to design and develop firmware for custom stm32 pcbs. leading can board and drs projects. implementing over 16 sensors in the car for data acquisition, test verification, and internal board communication. working along side several other subsystems to design the best car for michigan FSAE compitition. won 6th place last year out of 120 teams, hoping to do better this year we have alot in the works.",
+          "leading subteam of firmware engineers to design and develop firmware for custom stm32 pcbs. leading various firmware projects. working along side several other subsystems to design the best car for michigan FSAE compitition. won 6th place last year out of 120 teams, hoping to do better this year we have alot in the works.",
         achievements: [
           "led firmware workstreams + planning",
           "stm32 + can communication",
-          "sensor integration + hardware validation",
+          "sensors + drs",
         ],
         image: racing,
       },
