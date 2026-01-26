e5c2fc873480c9ed45b0ac32cf6b466a341996af
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-01-26 08:43:03 -0800

    Update WorkContent.tsx

--- src/components/content/WorkContent.tsx
+++ src/components/content/WorkContent.tsx
@@ -36,7 +36,7 @@ function WorkContent() {
         company: "calpoly racing",
         period: "2025 - now",
         description:
-          "leading subteam of firmware engineers to design and develop firmware for custom stm32 pcbs. leading various firmware projects. working along side several other subsystems to design the best car for michigan FSAE compitition. won 6th place last year out of 120 teams, hoping to do better this year we have alot in the works.",
+          "leading subteam of firmware engineers to design and develop firmware for custom stm32 pcbs. leading various firmware projects. working along side several other subsystems to design the best car for michigan FSAE compitition.",
         achievements: [
           "led firmware workstreams + planning",
           "stm32 + can communication",
