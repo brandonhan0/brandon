c5393d52b44de5f1c9418bc097fd51e16c9a7ac0
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-02-14 19:23:10 -0800

    Update FunContent.tsx

--- src/components/content/FunContent.tsx
+++ src/components/content/FunContent.tsx
@@ -11,7 +11,6 @@ import pic10 from "../../assets/images/image10.jpeg";
 import pic11 from "../../assets/images/image11.jpeg";
 import pic12 from "../../assets/images/LA.png";
 import pic13 from "../../assets/images/tahoe.png";
-import pic14 from "../../assets/images/IMG_5078.PNG";
 
 
 
@@ -37,7 +36,6 @@ export default function FunContent() {
   const interests: FunItem[] = useMemo(
     () => [
       { title: "tahoe", desc: "tahoe hike in the rainy storm", image: pic13 },
-      { title: "base", desc: "just upgraded to TH13", image: pic14 },
       { title: "la", desc: "i went to LA and saw some palm trees", image: pic12 },
       { title: "tkd", desc: "heres my family", image: pic11 },
       { title: "taiwan", desc: "am i a donut?", image: pic9 },
