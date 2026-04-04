495c6ab605827bc521838f63eb3b0cb14d223a6c
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-04-03 23:47:39 -0700

    Update Desktop.tsx

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -6,7 +6,7 @@ import car from "../assets/icons/poygon.gif";
 import spongebob from "../assets/icons/spongebob.gif";
 import charliebrown from "../assets/icons/car2.gif";
 //import bgImage from "../assets/backgrounds/background.png";
-import bgImage from "../assets/backgrounds/background.gof.gif";
+import bgImage from "../assets/backgrounds/background3.gif";
 
 
 interface DesktopProps {
