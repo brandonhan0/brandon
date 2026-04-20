7e230b576c5faaec5ed84dbc8833ab167393efb1
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-04-19 22:06:02 -0700

    Update ProjectsContent.tsx

--- src/components/content/ProjectsContent.tsx
+++ src/components/content/ProjectsContent.tsx
@@ -36,7 +36,7 @@ function ProjectsContent() {
   const projects: ProjectItem[] = useMemo(
     () => [
       {
-        title: "autonomous lost object finder",
+        title: "autonomous lost object finder robot",
         description: "made a robot that helps patients with dementia(or college students) find objects they lose around the house. did a study interviewing students and found that a common nuisance among them was they were always losing belongings. the solution i came up with was make an fully autonomous robot that can look for any item you have lost. uses ultrlytic object detection to identify objects, and will ping the user if the object is found. uses lidar for localization and mapping to autonomously navigate throughout a building or home. used ros2 to organize and execute code, this was very fun shoutout to ros2.",
         tech: ["NVIDIA jetson orin nano", "C", "ROS2", "lidar", "RGBD", "Ultrlytics", "YOLO"],
         images: [],
