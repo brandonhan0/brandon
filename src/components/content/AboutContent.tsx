16e340998571727c0f23785f19197b8bc6fe6070
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-04-20 00:43:24 -0700

    Update AboutContent.tsx

--- src/components/content/AboutContent.tsx
+++ src/components/content/AboutContent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import me from "../../assets/images/IMG_3672.jpg";
+import me from "../../assets/images/me.jpeg";
 import snake from "../../assets/images/image3.jpeg";
 
 
